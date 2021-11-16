d57e5b871f7d9bbdc00b5550c850bce0c8c0c9ce
Szabolcs Garda <gardas86@gmail.com>
2021-11-16 05:18:59 +0100

    Analysis in progress

--- src/dtu/Main.java
+++ src/dtu/Main.java
@@ -5,15 +5,13 @@ import dtu.expressions.Expression;
 import java.util.HashMap;
 
 public class Main {
+	public static void main(String[] args)
+	{
+		System.out.println("Initialization in progress");
+		//TODO: Place decent graph builder here
 
-    public static void main(String[] args) {
-	System.out.println("Initialization in progress");
-
-	//TODO: Place decent graph builder here
-	HashMap<String, Expression> dummyExpressions = new HashMap<>();
-
-
-	ProgramGraph mProgramGraph = new ProgramGraph(5, dummyExpressions);
+		HashMap<String, Expression> dummyExpressions = new HashMap<>();
+		ProgramGraph mProgramGraph = new ProgramGraph(5, dummyExpressions);
 
     }
 }
--- src/dtu/analysis/Analysis.java
+++ src/dtu/analysis/Analysis.java
@@ -1,4 +1,28 @@
 package dtu.analysis;
 
+import dtu.ProgramGraph;
+import dtu.expressions.Expression;
+
+import java.util.ArrayList;
+
 public class Analysis {
+
+    public Analysis()
+    {
+
+    }
+
+    public ArrayList<Expression>generateWorkList(ProgramGraph aProgramGraph)
+    {
+        System.out.println();
+        ArrayList<Expression> result = new ArrayList<>();
+        int selectedNode;
+        boolean finished = false;
+
+        while(!finished)
+        {
+
+        }
+        return result;
+    }
 }
